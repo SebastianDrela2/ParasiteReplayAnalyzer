141446417fa999e46d8b980807cc06c55e074835
Sebastian Drela <sebastian.drela@wp.pl>
2023-09-16 03:09:54 +0200

    modified ignore, added settings ui

---
+++ UI/SettingsUI.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using ParasiteReplayAnalyzer.Saving;
+
+namespace ParasiteReplayAnalyzer.UI
+{
+    /// <summary>
+    /// Interaction logic for SettingsUI.xaml
+    /// </summary>
+    public partial class SettingsUI : Window
+    {
+        private SettingsManager _settingsManager;
+        private ParasiteMainUiWindow _mainUiWindow;
+        public SettingsUI(SettingsManager settingsManager, ParasiteMainUiWindow mainUiWindow)
+        {
+            InitializeComponent();
+            Show();
+
+            _mainUiWindow = mainUiWindow;
+            _settingsManager = settingsManager;
+        }
+
+        private void OnOkClicked(object sender, RoutedEventArgs e)
+        {
+            var newReplaysPath = _pathTextBox.Text;
+
+            _settingsManager.SaveSettings(newReplaysPath);
+            _settingsManager.LoadSettings();
+
+            _mainUiWindow.LoadReplaysAsync();
+            _mainUiWindow.FillListBoxItems();
+
+            Close();
+        }
+    }
+}
